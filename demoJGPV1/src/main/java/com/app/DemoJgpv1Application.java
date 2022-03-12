4c9723b7e2faf97b53f5528fd9fb694152bfe67a
perezjuang <perezjuang@hotmail.com>
2022-03-12 09:23:48 -0500

    Creacion del Proyecto

---
+++ demoJGPV1/src/main/java/com/app/DemoJgpv1Application.java
@@ -0,0 +1,13 @@
+package com.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoJgpv1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoJgpv1Application.class, args);
+	}
+
+}
