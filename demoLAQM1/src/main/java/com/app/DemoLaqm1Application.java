9e40b6f1ce7e9baa4a84649f02c0c8c34da8e352
Luis Quimbayo Marin <37000727+lmxter@users.noreply.github.com>
2022-03-12 09:34:53 -0500

    commit demo

---
+++ demoLAQM1/src/main/java/com/app/DemoLaqm1Application.java
@@ -0,0 +1,13 @@
+package com.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoLaqm1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoLaqm1Application.class, args);
+	}
+
+}
