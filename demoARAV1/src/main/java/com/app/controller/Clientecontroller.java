ff1a80b8d41ac8b9a0f21ac445650b93ab7c7d41
Alejandro_Restrepo <alejorestre004@gmail.com>
2022-04-23 08:19:59 -0500

    controller_actualizado

--- demoARAV1/src/main/java/com/app/controller/Clientecontroller.java
+++ demoARAV1/src/main/java/com/app/controller/Clientecontroller.java
@@ -6,7 +6,12 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.app.entity.Cliente;
@@ -26,5 +31,27 @@ public class Clientecontroller {
 		return ClienteRepository.findAll();
     }
 	
+	
+	@CrossOrigin(origins = "*")
+	@PostMapping(path = "/Clienteadd", consumes = "application/json", produces = "application/json")
+	public Cliente addNewCliente(@RequestBody Cliente cliente) {
+		cliente = ClienteRepository.save(cliente);
+		return cliente;
+	}
+	
+	
+	@CrossOrigin(origins = "*")
+	@PutMapping(path = "/Clienteupdate")
+	public Cliente updateCliente(@RequestBody Cliente cliente) {
+		ClienteRepository.save(cliente);
+		return cliente;
+	}
+	
+	@CrossOrigin(origins = "*")
+	@DeleteMapping(path = "/Clientedelete")
+	public String delenteCliente(Long id) {
+		ClienteRepository.deleteById(id);
+		return "eliminado";
+	}
 
 }
--- demoARAV1/src/main/java/com/app/repository/ClienteRepository.java
+++ demoARAV1/src/main/java/com/app/repository/ClienteRepository.java
@@ -1,11 +1,17 @@
 package com.app.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.app.entity.*;
 
 @Repository
 public interface ClienteRepository extends JpaRepository<Cliente,Long> {
+
+	@Query(value = "select * from cliente where id=?", nativeQuery = true)
+	public List<Cliente> findByCliente(Integer idCliente);
 
 }
